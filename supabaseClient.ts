b73929980fa36237f7910d0919dba7386a16dc99
fpfinfo <fpf.info@gmail.com>
2026-02-04 23:37:51 -0300

    Configure Supabase integration via environment variables

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xxvmpbjgvpxumdbvlufl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dm1wYmpndnB4dW1kYnZsdWZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNDI3NDYsImV4cCI6MjA4NTgxODc0Nn0.danQ23xbtuNg-w_bvBWWuOed-9wjC3X55p7br_EWtfg';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
